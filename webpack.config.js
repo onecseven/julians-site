47d8413b774408093c92b8de29c8d295d113d7e5
Your Name <iced.tati@gmail.com>
2021-06-09 16:38:34 -0400

    config

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,10 @@ module.exports = (env, argv) => {
         {
           test: /\.scss$/,
           use: ["style-loader", "css-loader", "sass-loader",],
+        },
+        {
+          test: /\.(png|jpg)$/,
+          loader: 'url-loader'
         }
       ],      
     },
