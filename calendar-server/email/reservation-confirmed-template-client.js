7738c57164d5203e48c810c116899a884dd542bb
Your Name <iced.tati@gmail.com>
2021-07-16 17:31:05 -0400

    better templating

--- calendar-server/email/reservation-confirmed-template-client.js
+++ calendar-server/email/reservation-confirmed-template-client.js
@@ -1,27 +1,42 @@
 let {user} = require("../auth/email")
 
+const ENERGY = "Guided Energy Work"
+const YOGA = "Yoga for Embodiment and Transformation" 
+
 
 /**
  * 
  * @param clientEmail string 
- * @param clientName string
- * @param reservationDate Date
+ * @param clientFirstName string
+ * @param clientLastName string
+ * @param date Date
+ * @param meetingType "ENERGY" / "YOGA"
  * @returns An object ready to be sent with nodemailer notifying the client that their
  * reservation has been notes and taken.
  */
 
-exports.confirmationTemplate = ({clientEmail, clientName, reservationDate}) => {
+exports.confirmationTemplate = ({clientEmail, clientFirstName,clientLastName, meetingType, date, meetingDuration}) => {
   //need to switch on meeting type refere to doc
-
+  const meetingTypeStr = meetingType === "YOGA" ? YOGA : meetingType === "ENERGY" ? ENERGY : "Appointment"
+  let meetingDurationStr
+  switch(meetingDuration){
+    case 60: {
+      meetingDurationStr = `1 hour`
+      break
+    }
+    default:{
+      meetingDurationStr = `${meetingDuration}m`
+    }
+  }
   return {
     from: user,
     to: clientEmail,
     subject: "test",
-    text: `Hi, 
-    Thanks for scheduling with us. This email serves as a confirmation that your appointment for [time and type of appointment (ie 30 minute Guided Energy Work)] at [date and time] has been approved. You will receive a questionnaire [and waiver if for yoga] soon so I can best prepare for our work together. If you need to change or cancel our appointment, please to the best of your ability email me within 24 hours of your scheduled time. 
+    text: `Hi ${clientFirstName}, 
+    Thanks for scheduling with us. This email serves as a confirmation that your appointment for your ${meetingTypeStr} (${meetingDurationStr}) at ${new Date().toString()} has been approved. You will receive a questionnaire ${meetingType === "YOGA" ? "and waiver" : ""} soon so I can best prepare for our work together. If you need to change or cancel our appointment, please to the best of your ability email me within 24 hours of your scheduled time. 
     With loving kindness,
     Julian
+    radfairyflows    
     `,
-    // html: "<p>HTML version of the message</p>",
   }
 }
