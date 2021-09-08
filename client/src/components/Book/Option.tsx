d153cb256e30030079bdf51bfea444d70d058c34
Your Name <iced.tati@gmail.com>
2021-09-08 14:42:08 -0400

    beginning to create the forms

---
+++ client/src/components/Book/Option.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+
+export const Option = ({name, description}) => {
+  return (
+  <div className="option-member">
+    <h2>
+      name
+    </h2>
+    <p>
+      description
+    </p>
+  </div>
+  )
+}
---
+++ client/src/components/Book/OptionsDisplay.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export const OptionsDisplay = (props) => {
+  return (
+  <div className="options-container">
+    
+  </div>
+  )
+}
