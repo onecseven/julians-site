4f9ba4dc496b005b5ad9cc941400b595248026cc
Your Name <iced.tati@gmail.com>
2021-09-10 17:59:25 -0400

    added props type declaration

--- client/src/components/Book/Option.tsx
+++ client/src/components/Book/Option.tsx
@@ -1,10 +1,19 @@
 import React from "react"
 
-export const Option = ({ name, description }) => {
+declare interface optionProps {
+  name: string
+  description: string
+  duration: number[]
+}
+
+export const Option = ({ name, description, duration}) => {
   return (
     <div className="option-member">
       <h2>{name}</h2>
       <p>{description} </p>
+      <ul>
+        {duration.map(minutes => (<li>{minutes}</li>))}
+      </ul>
     </div>
   )
 }
