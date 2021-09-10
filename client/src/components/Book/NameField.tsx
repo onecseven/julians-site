40d81e2b14b736f8d49de26fb3eadbd54a046501
Your Name <iced.tati@gmail.com>
2021-09-10 17:58:19 -0400

    input boxes

---
+++ client/src/components/Book/NameField.tsx
@@ -0,0 +1,18 @@
+import React, {useState} from "react"
+import {TextInput} from "../shared/InputBox"
+
+export const NameField = (props) => {
+  const [value, setValue] = useState("")
+
+  return (
+    <TextInput
+    onChange={(newVal) => setValue(newVal)}
+    placeholder="What's your name?"
+    value={value}
+    />
+  )
+}
+// TODO
+// 1. CREATE INPUT FIELD
+// 2. USE HOOKS TO MANAGE IT
+// 3. CONNECT IT TO REDUX
