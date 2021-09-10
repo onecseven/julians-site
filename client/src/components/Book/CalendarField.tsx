2110f4a290f460874d9f5ea3d44085da6753b45e
Your Name <iced.tati@gmail.com>
2021-09-10 17:58:08 -0400

    placeholder component, need to do this one

---
+++ client/src/components/Book/CalendarField.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export const CalendarField = (props) => {
+  return (<div/>)
+}
+  // TODO
+  // 1. CREATE INPUT FIELD
+  // 2. USE HOOKS TO MANAGE IT
+  // 3. CONNECT IT TO REDUX
