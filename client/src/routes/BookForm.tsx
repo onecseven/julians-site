c0e4e8672f584816f22ecdc0ddcca62bd6eed7c0
Your Name <iced.tati@gmail.com>
2021-09-14 15:23:13 -0400

    todo + css

--- client/src/routes/BookForm.tsx
+++ client/src/routes/BookForm.tsx
@@ -1,16 +1,38 @@
+import { makeStyles, Paper } from "@material-ui/core"
 import React from "react"
 import { CalendarField } from "../components/Book/CalendarField"
 import { EmailField } from "../components/Book/EmailField"
 import { NameField } from "../components/Book/NameField"
 import { OptionsField } from "../components/Book/OptionsField"
 
+const useStyles = makeStyles((theme) => ({
+  papier: {
+    width: "60%",
+    maxWidth: 480,
+    backgroundColor: "#f3f1d1",
+    marginLeft: "25%",
+    marginRight: "25%",
+    marginTop: "2%",
+    textAlign: "center",
+   },
+}))
+
+
+
 export const BookForm = (props) => {
+  const classes = useStyles()
+
   return (
-      <div className="options-display">
+    <Paper elevation={3} className={classes.papier}>
         <OptionsField/> 
-        <NameField/>
+        {/* <NameField/>
         <EmailField/>
-        <CalendarField/> 
-      </div>
+        <CalendarField/>  */}
+    </Paper>
   )
 }
+
+//TODO
+//1. Button underneath length for booking.
+//2. New screen for booking, with just name field, email field, calendar and available times.
+//3. Then a button for sending the reservation
