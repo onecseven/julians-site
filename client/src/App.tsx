d580253c84901bd553e145f1cdeb54cc7960cf7c
Your Name <iced.tati@gmail.com>
2021-09-17 15:19:49 -0400

    made things better

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,29 +2,45 @@ import React, { Component } from "react"
 import { hot } from "react-hot-loader"
 import { Provider } from "react-redux"
 import { Navbar } from "./components/Navbar"
-import {store} from "./store/store"
+import { store } from "./store/store"
 import "./App.css"
 import { Router, Link } from "@reach/router"
 import { Contact } from "./routes/Contact"
 import { About } from "./routes/About"
-import { Services } from "./routes/BookForm"
+import { OptionsField as Services } from "./routes/OptionsField"
+import { Book } from "./routes/Book"
+import { makeStyles, Paper } from "@material-ui/core"
 
+const useStyles = makeStyles((theme) => ({
+  papier: {
+    width: "60%",
+    maxWidth: 480,
+    backgroundColor: "#f3f1d1",
+    marginLeft: "25%",
+    marginRight: "25%",
+    marginTop: "2%",
+    textAlign: "center",
+  },
+}))
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Provider store={store}>
-          <Navbar />
+const App = () => {
+  const classes = useStyles()
+
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Navbar />
+        <Paper elevation={3} className={classes.papier}>
           <Router>
             <Contact path="contact" />
-            <About path="about"/>
-            <Services path="services"/>
+            <About path="about" />
+            <Services path="services" />
+            <Book path="book/:service" />
           </Router>
-        </Provider>
-      </div>
-    )
-  }
+        </Paper>
+      </Provider>
+    </div>
+  )
 }
 
 export default hot(module)(App)
