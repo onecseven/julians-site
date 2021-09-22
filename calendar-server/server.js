3662a15479cbdaff74f7750b2d97485637c2c513
Your Name <iced.tati@gmail.com>
2021-09-22 14:01:06 -0400

    getting rid of mongo

--- calendar-server/server.js
+++ calendar-server/server.js
@@ -1,20 +1,9 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const CalendarRouter = require("./routes/CalendarRoutes.js");
-const {user, password} = require("./auth/db")
 
 const app = express()
 app.use(express.json())
-
-
-mongoose.connect(
-  `mongodb+srv://${user}:${password}@cluster0.sutow.mongodb.net/calendar?retryWrites=true&w=majority`,
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  }
-)
 
 app.use(CalendarRouter);
 
