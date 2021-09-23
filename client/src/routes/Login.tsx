a584cc890dc02c7fc2032d2b164cd202357cd46a
Your Name <iced.tati@gmail.com>
2021-09-23 18:22:17 -0400

    new views

---
+++ client/src/routes/Login.tsx
@@ -0,0 +1,55 @@
+import { Button, Checkbox, FormControlLabel, TextField } from "@material-ui/core"
+import React, { useState } from "react"
+import { useDispatch } from "react-redux"
+import { StartLogin } from "../store/loginSlice"
+
+export const Login = (props) => {
+  const [email, setEmail] = useState("")
+  const [pwd, setPwd] = useState("")
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <form
+        className="loginForm"
+        onSubmit={(event) => {
+          event.preventDefault()
+          dispatch(StartLogin({ email, password: pwd }))
+        }}
+      >
+        <TextField
+          id="email"
+          label="Email"
+          value={email}
+          onChange={(event) => {
+            let newText = event.target.value
+            setEmail(newText)
+          }}
+          variant="filled"
+        />
+        <div className="br" />
+        <TextField
+          id="password"
+          type="password"
+          label="Password"
+          value={pwd}
+          onChange={(event) => {
+            let newText = event.target.value
+            setPwd(newText)
+          }}
+          variant="filled"
+        />
+        <FormControlLabel control={<Checkbox />} label="Keep Me Logged In" />
+        <Button
+          size="large"
+          variant="outlined"
+          type="submit"
+          color="primary"
+          id="form_button"
+        >
+          Log in
+        </Button>
+      </form>
+    </div>
+  )
+}
---
+++ client/src/routes/Register.tsx
@@ -0,0 +1,67 @@
+import { Button, Checkbox, FormControlLabel, TextField } from "@material-ui/core"
+import React, { useState } from "react"
+import { useDispatch } from "react-redux"
+import { StartRegistration } from "../store/loginSlice"
+
+const label = { inputProps: { "aria-label": "Keep Logged In" } }
+
+export const Register = (props) => {
+  const [email, setEmail] = useState("")
+  const [name, setName] = useState("")
+  const [pwd, setPwd] = useState("")
+  const dispatch = useDispatch()
+
+  return (
+    <div>
+      <form
+        className="loginForm"
+        onSubmit={(event) => {
+          event.preventDefault()
+          dispatch(StartRegistration({ email, password: pwd }))
+        }}
+      >
+        <TextField
+          id="name"
+          label="name"
+          value={name}
+          onChange={(event) => {
+            let newText = event.target.value
+            setName(newText)
+          }}
+          variant="filled"
+        />
+        <TextField
+          id="email"
+          label="Email"
+          value={email}
+          onChange={(event) => {
+            let newText = event.target.value
+            setEmail(newText)
+          }}
+          variant="filled"
+        />
+        <div className="br" />
+        <TextField
+          id="password"
+          type="password"
+          label="Password"
+          value={pwd}
+          onChange={(event) => {
+            let newText = event.target.value
+            setPwd(newText)
+          }}
+          variant="filled"
+        />
+        <Button
+          size="large"
+          variant="outlined"
+          type="submit"
+          color="primary"
+          id="form_button"
+        >
+          Log in
+        </Button>
+      </form>
+    </div>
+  )
+}
