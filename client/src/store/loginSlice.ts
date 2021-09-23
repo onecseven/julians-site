4aa1f0ba7b7d56a06135b2f383bc2a374052cdc8
Your Name <iced.tati@gmail.com>
2021-09-23 18:22:36 -0400

    scaffolding

---
+++ client/src/store/loginSlice.ts
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+type initialLoginState = {
+  email: String,
+  password: String
+}
+
+const initialState: initialLoginState = {
+  email: '',
+  password: ''
+}
+
+export const loginSlice = createSlice({
+  name: "login",
+  initialState,
+  reducers: {
+    StartLogin: (state, action) => {
+    },
+    StartRegistration: (state, action) => {
+
+    }
+  },
+})
+
+export const {
+  StartLogin,
+  StartRegistration
+} = loginSlice.actions
+
+export default loginSlice.reducer
