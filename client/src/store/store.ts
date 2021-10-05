d3a05b4539e36311854e3b69f0fdfbcd5f2fbee7
Your Name <iced.tati@gmail.com>
2021-10-04 20:38:32 -0400

    store

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,11 +1,38 @@
-import { configureStore, ThunkAction } from '@reduxjs/toolkit'
-import formSlice from './formSlice'
+import {
+  configureStore,
+  getDefaultMiddleware,
+  MiddlewareArray,
+} from "@reduxjs/toolkit"
+import formSlice from "./slices/formSlice"
+import { createLogicMiddleware } from "redux-logic"
+import { POST_LOGIN_LOGIC } from "./logic/loginLogic"
+import axios from "axios"
+import calendarSlice from "./slices/calendarSlice"
+import userSlice from "./slices/userSlice"
+
+const deps = {
+  httpClient: axios,
+}
+
+const logicMiddleware = createLogicMiddleware([POST_LOGIN_LOGIC], deps)
+
+const middleware = getDefaultMiddleware().concat(logicMiddleware)
+
+export const fakeStore = configureStore({
+  reducer: {
+    form: formSlice,
+    calendar: calendarSlice,
+    user: userSlice,
+  },
+})
 
 export const store = configureStore({
   reducer: {
-    form: formSlice
-  }
+    form: formSlice,
+  },
+  middleware: middleware,
 })
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
+export type FakeRootState = ReturnType<typeof fakeStore.getState>
