3fb74e297545387d3fb0dfd448c521d6b049f925
Your Name <iced.tati@gmail.com>
2021-10-04 20:37:40 -0400

    NEW TYPES

--- client/types.d.ts
+++ client/types.d.ts
@@ -2,3 +2,21 @@ declare interface meetingType {
   duration: number
   name: "YOGA" | "ENERGY"
 }
+
+declare type action = {
+  type: string
+  payload: any
+}
+
+declare namespace ActionTypes {
+  interface POST_LOGIN extends action {
+    type: "POST_LOGIN"
+    payload: {
+      password: string
+      email: string
+    }
+  }
+  interface LOGIN_SUCCESS extends action {
+    type: "LOGIN_SUCCESS"
+  }
+}
