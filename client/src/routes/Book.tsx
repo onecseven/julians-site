825dafea41e28580f46635fa6e883cb340f9b9a3
Your Name <iced.tati@gmail.com>
2021-10-05 16:29:32 -0400

    timeslot implemented

--- client/src/routes/Book.tsx
+++ client/src/routes/Book.tsx
@@ -1,19 +1,28 @@
 import React from "react"
-import { useDispatch } from "react-redux"
+import {
+  useAppDispatch as useDispatch,
+  useAppSelector as useSelector,
+} from "../store/hooks"
 import { CalendarField } from "../components/Book/CalendarField"
 import { EmailField } from "../components/Book/EmailField"
 import { NameField } from "../components/Book/NameField"
 import { setMeetingType } from "../store/slices/formSlice"
+import { TimeslotTest } from "../components/Test/TimeslotTest"
+
+//TODO IMPLEMENT ERROR
 
 export const Book = (props) => {
   let { service } = props
+  const { timeslots, error } = useSelector((state) => state.calendar)
   const dispatch = useDispatch()
   dispatch(setMeetingType(service.toUpperCase()))
+  // if (error) ?
   return (
     <>
       <NameField />
       <EmailField />
       <CalendarField />
+      {(timeslots && timeslots.length > 0) ? <TimeslotTest timeslot={timeslots} /> : null}
     </>
   )
 }
