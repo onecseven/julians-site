9b9ddd3e5368cb45be8a0db7ee0f30b02ed10ef4
Your Name <iced.tati@gmail.com>
2021-10-05 16:29:17 -0400

    todo

--- client/src/components/Book/EmailField.tsx
+++ client/src/components/Book/EmailField.tsx
@@ -30,8 +30,3 @@ export const EmailField = (props) => {
     />
   )
 }
-
-// TODO
-// 1. CREATE INPUT FIELD
-// 2. USE HOOKS TO MANAGE IT
-// 3. CONNECT IT TO REDUX
--- client/src/components/Book/NameField.tsx
+++ client/src/components/Book/NameField.tsx
@@ -32,7 +32,3 @@ export const NameField = (props) => {
     />
   )
 }
-// TODO
-// 1. CREATE INPUT FIELD
-// 2. USE HOOKS TO MANAGE IT
-// 3. CONNECT IT TO REDUX
