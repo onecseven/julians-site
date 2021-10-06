fb6b10786249ee7b71c0064b028517c3e50be0b0
Your Name <iced.tati@gmail.com>
2021-10-06 16:33:18 -0400

    hell yeah!!

--- client/src/store/slices/userSlice.ts
+++ client/src/store/slices/userSlice.ts
@@ -1,29 +1,41 @@
 import { createAction, createSlice } from "@reduxjs/toolkit"
-
+import { navigate } from "@reach/router"
 // TODO fill this in
 
 export const POST_CREATE_USER =
-  createAction<{ email: string; password: string }>("POST_LOGIN")
+  createAction<{ email: string; password: string; name: string }>(
+    "user/POST_CREATE_USER"
+  )
 
-export const CREATE_USER_FAILURE =
-  createAction<{ error: string }>("LOGIN_FAILURE")
+export const CREATE_USER_SUCCESS = createAction<{
+  email: string
+  pwd: string
+  user_id: number
+}>("user/CREATE_USER_SUCCESS")
 
-export const CREATE_USER_SUCCESS =
-  createAction<{ email: string; pwd: string; user_id: number }>("LOGIN_SUCCESS")
+export const CREATE_USER_FAILURE = createAction<{ error: string }>(
+  "user/CREATE_USER_FAILURE"
+)
 
-type initialUserState = {
-}
+type initialUserState = {}
 
-const initialState: initialUserState = {
-}
+const initialState: initialUserState = {}
 
 export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
     POST_CREATE_USER: (state, action) => {},
-    CREATE_USER_SUCCESS: (state, action) => {},
-    CREATE_USER_FAILURE: (state, action) => {},
+    CREATE_USER_SUCCESS: (state, action) => {
+      console.log("create user success")
+      console.dir(action.payload)
+      navigate("/")
+
+    },
+    CREATE_USER_FAILURE: (state, action) => {
+      console.log("create user failure")
+      console.dir(action.payload)
+    },
   },
 })
 
