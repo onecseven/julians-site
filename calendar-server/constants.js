077649cefe2c736954da8ad3be9038c2f166f6e0
Your Name <iced.tati@gmail.com>
2021-10-15 17:24:13 -0400

    no more types in comments plz

---
+++ calendar-server/constants.js
@@ -0,0 +1,23 @@
+module.exports = {
+  order: {
+    meeting_type: {
+      yoga: "YOGA",
+      energy: "ENERGY",
+      consult: "CONSULT",
+    },
+    dateConverter: (date) => {
+      const yourDate = new Date(date)
+      return yourDate.toISOString().split('T')[0]
+    },
+    timeslotConverter: (num) => {
+      null
+      return null
+    },
+    order_status: {
+      awaiting: "AWAITING_APPROVAL",
+      upcoming: "UPCOMING",
+      completed: "COMPLETED",
+      canceled: "CANCELED"
+    }
+  },
+}
