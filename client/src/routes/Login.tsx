dc8d72d6ff3009ba6e4340d45e02dd56af7360b3
Your Name <iced.tati@gmail.com>
2021-10-19 20:47:53 -0400

    basic error handling

--- client/src/routes/Login.tsx
+++ client/src/routes/Login.tsx
@@ -3,7 +3,11 @@ import {
   Checkbox,
   FormControlLabel,
   TextField,
-} from "@material-ui/core"
+  Stack,
+  Alert,
+  AlertTitle,
+  Box,
+} from "@mui/material"
 import React, { useState } from "react"
 import { useDispatch } from "react-redux"
 import { POST_LOGIN } from "../store/features/login/loginActions"
@@ -12,7 +16,14 @@ export const Login = (props) => {
   const [email, setEmail] = useState("")
   const [pwd, setPwd] = useState("")
   const dispatch = useDispatch()
-
+  let error = props["*"]
+  let alert =
+    error === "error" ? (
+      <Alert severity="error">
+        <AlertTitle>Error</AlertTitle>
+        There has been an issue logging in. Please try again.
+      </Alert>
+    ) : null
   return (
     <div>
       <form
@@ -22,38 +33,46 @@ export const Login = (props) => {
           dispatch(POST_LOGIN({ email, password: pwd }))
         }}
       >
-        <TextField
-          id="email"
-          label="Email"
-          value={email}
-          onChange={(event) => {
-            let newText = event.target.value
-            setEmail(newText)
-          }}
-          variant="filled"
-        />
-        <div className="br" />
-        <TextField
-          id="password"
-          type="password"
-          label="Password"
-          value={pwd}
-          onChange={(event) => {
-            let newText = event.target.value
-            setPwd(newText)
-          }}
-          variant="filled"
-        />
-        <FormControlLabel control={<Checkbox />} label="Keep Me Logged In" />
-        <Button
-          size="large"
-          variant="outlined"
-          type="submit"
-          color="primary"
-          id="form_button"
-        >
-          Log in
-        </Button>
+        <Box>
+          <Stack spacing={2}>
+            {alert}
+            <TextField
+              id="email"
+              label="Email"
+              value={email}
+              onChange={(event) => {
+                let newText = event.target.value
+                setEmail(newText)
+              }}
+              variant="filled"
+            />
+            <div className="br" />
+            <TextField
+              id="password"
+              type="password"
+              label="Password"
+              value={pwd}
+              onChange={(event) => {
+                let newText = event.target.value
+                setPwd(newText)
+              }}
+              variant="filled"
+            />
+            <FormControlLabel
+              control={<Checkbox />}
+              label="Keep Me Logged In"
+            />
+            <Button
+              size="large"
+              variant="outlined"
+              type="submit"
+              color="primary"
+              id="form_button"
+            >
+              Log in
+            </Button>
+          </Stack>
+        </Box>
       </form>
     </div>
   )
--- client/src/routes/Register.tsx
+++ client/src/routes/Register.tsx
@@ -1,87 +1,128 @@
-import {
-  Button,
-  Checkbox,
-  FormControlLabel,
-  TextField,
-} from "@material-ui/core"
+import { AccountCircle, Email, Lock } from "@mui/icons-material"
+import { Button, TextField, Stack, Alert, AlertTitle } from "@mui/material"
+import { Box } from "@mui/system"
 import React, { useState } from "react"
 import { useDispatch } from "react-redux"
 import { POST_CREATE_USER } from "../store/features/user/userSlice"
 
-const label = { inputProps: { "aria-label": "Keep Logged In" } }
-
+// const label = { inputProps: { "aria-label": "Keep Logged In" } }
 
 export const Register = (props) => {
   const [email, setEmail] = useState("")
   const [name, setName] = useState("")
   const [pwd, setPwd] = useState("")
   const [pwd2, setPwd2] = useState("")
-  let error = pwd !== pwd2
-
+  const [seeAlert, setAlert] = useState(false)
+  // let error = pwd !== pwd2
+  let error = true
+  let alert = (
+    <Alert severity="warning" sx={{width: "90%", ml: 3.5}}>
+      <AlertTitle>Warning</AlertTitle>
+      Passwords don't match.
+    </Alert>
+  )
   const dispatch = useDispatch()
   return (
     <div>
       <form
         className="loginForm"
         onSubmit={(event) => {
+          if (error) {
+            setAlert(true)
+            event.preventDefault()
+            return
+          }
           event.preventDefault()
           dispatch(POST_CREATE_USER({ email, password: pwd, name }))
         }}
       >
-        <TextField
-          id="name"
-          label="name"
-          value={name}
-          onChange={(event) => {
-            let newText = event.target.value
-            setName(newText)
-          }}
-          variant="filled"
-        />
-        <TextField
-          id="email"
-          label="Email"
-          value={email}
-          onChange={(event) => {
-            let newText = event.target.value
-            setEmail(newText)
-          }}
-          variant="filled"
-        />
-        <div className="br" />
-        <TextField
-          id="password"
-          type="password"
-          label="Password"
-          value={pwd}
-          onChange={(event) => {
-            let newText = event.target.value
-            setPwd(newText)
-          }}
-          error={error}
-          variant="filled"
-        />
-        <TextField
-          id="password2"
-          type="password"
-          label="Password"
-          value={pwd2}
-          onChange={(event) => {
-            let newText = event.target.value
-            setPwd2(newText)
-          }}
-          error={error}
-          variant="filled"
-        />
-        <Button
-          size="large"
-          variant="outlined"
-          type="submit"
-          color="primary"
-          id="form_button"
-        >
-          Register
-        </Button>
+        <Stack spacing={2.5}>
+          <Box>{error ? alert : null}</Box>
+          <Box sx={{ display: "inline-flex" }}>
+            <AccountCircle
+              fontSize="large"
+              sx={{ color: "action.active", mr: 1, my: 2 }}
+            />
+            <TextField
+              id="name"
+              label="Name"
+              value={name}
+              onChange={(event) => {
+                let newText = event.target.value
+                setName(newText)
+              }}
+              variant="filled"
+              sx={{ width: "100%" }}
+            />
+          </Box>
+          <Box sx={{ display: "inline-flex" }}>
+            <Email
+              fontSize="large"
+              sx={{ color: "action.active", mr: 1, my: 2 }}
+            />
+            <TextField
+              id="email"
+              label="Email"
+              value={email}
+              onChange={(event) => {
+                let newText = event.target.value
+                setEmail(newText)
+              }}
+              variant="filled"
+              sx={{ width: "100%" }}
+            />
+          </Box>
+          <Box sx={{ display: "inline-flex" }}>
+            <Lock
+              fontSize="large"
+              sx={{ color: "action.active", mr: 1, my: 2 }}
+            />
+
+            <TextField
+              id="password"
+              type="password"
+              label="Password"
+              sx={{ width: "100%" }}
+              value={pwd}
+              onChange={(event) => {
+                let newText = event.target.value
+                setPwd(newText)
+              }}
+              error={error}
+              variant="filled"
+            />
+          </Box>
+          <Box sx={{ display: "inline-flex" }}>
+            <Lock
+              fontSize="large"
+              sx={{ color: "action.active", mr: 1, my: 2 }}
+            />
+            <TextField
+              id="password2"
+              type="password"
+              label="Confirm password"
+              sx={{ width: "100%" }}
+              value={pwd2}
+              onChange={(event) => {
+                let newText = event.target.value
+                setPwd2(newText)
+              }}
+              error={error}
+              variant="filled"
+            />
+          </Box>
+          <Box>
+            <Button
+              size="large"
+              variant="outlined"
+              type="submit"
+              color="primary"
+              id="form_button"
+            >
+              Register
+            </Button>
+          </Box>
+        </Stack>
       </form>
     </div>
   )
