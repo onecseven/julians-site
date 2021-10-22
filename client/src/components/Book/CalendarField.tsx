758a5a1281001c749468892ef819640a17ce1358
Your Name <iced.tati@gmail.com>
2021-10-21 22:09:24 -0400

    forgot to dispatch the right actions

--- client/src/components/Book/CalendarField.tsx
+++ client/src/components/Book/CalendarField.tsx
@@ -1,7 +1,11 @@
 import React from "react"
 import Calendar from "react-calendar-mobile"
 import { useDispatch } from "react-redux"
-import { CLEAR_TIMESLOTS, POST_DATE } from "../../store/features/calendar/calendarActions"
+import {
+  CLEAR_TIMESLOTS,
+  POST_DATE,
+} from "../../store/features/calendar/calendarActions"
+import { setDate } from "../../store/features/form/formSlice"
 import { dateConverter } from "../shared/dateConverter"
 
 export const CalendarField = (props) => {
@@ -12,6 +16,7 @@ export const CalendarField = (props) => {
         onSelectDate={(newDate) => {
           dispatch(CLEAR_TIMESLOTS())
           dispatch(POST_DATE({ date: dateConverter(newDate) }))
+          dispatch(setDate({ date: dateConverter(newDate) }))
         }}
       />
     </div>
