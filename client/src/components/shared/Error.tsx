f4a85f7896890f9651ae2a5aa8bc3d657647d5c0
Your Name <iced.tati@gmail.com>
2021-10-22 18:38:05 -0400

    feature

--- client/src/components/shared/Error.tsx
+++ client/src/components/shared/Error.tsx
@@ -1,14 +1,19 @@
 import { Alert, AlertTitle } from "@mui/material"
 import React from "react"
 import { Link } from "@reach/router"
-
+import {
+  useAppDispatch as useDispatch,
+  useAppSelector as useSelector,
+} from "../../store/hooks"
 
 export const ErrorPage = (props) => {
+  const error = useSelector((state) => state.ui.error)
+  const fallback = "There's been an error!"
   return (
     <div>
       <Alert severity="error">
         <AlertTitle>Error</AlertTitle>
-        There's been an error!
+        {error ? error : fallback}
       </Alert>
       <Link to="/">Go back.</Link>
     </div>
