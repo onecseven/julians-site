dfec8c975625e07a52ec9e7fb250cf914640c4a3
Your Name <iced.tati@gmail.com>
2021-10-25 21:03:45 -0400

    TODO

--- client/src/components/shared/dateConverter.tsx
+++ client/src/components/shared/dateConverter.tsx
@@ -1,3 +1,24 @@
-export const dateConverter = (date: Date) => {
-  return date.toISOString().split("T")[0]
-}
+import calendarSlice from "../../store/features/calendar/calendarSlice";
+
+const dateToTime = date => date.toLocaleString('en-US', {
+  hour: 'numeric',
+  minute: 'numeric'
+});
+
+const dateString = "2019-05-05T10:30:00Z";
+const userOffset = new Date().getTimezoneOffset()*60*1000;
+const localDate = new Date(dateString);
+const utcDate = new Date(localDate.getTime() + userOffset);
+
+console.log(`${dateToTime(utcDate)} (${dateToTime(localDate)} Your Time)`);
+
+Mon-Friday 
+
+2-3pm
+3-4pm
+4-5pm
+5-6pm
+
+TODO timeslot converter (30minute blocks)
+TODO timeslot converter in emails and calendar (server)
+TODO disable weekends on calendars
